ffdda9b2d7cf53d76a5db2f3aff551e2d520ea7f
GONRONDEROS <gonzalorond@gmail.com>
2026-01-21 19:28:11 -0300

    corrigiendo nombre de PO checkout

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -3,7 +3,7 @@ const { Assert } = require('node:assert');
 const {LoginPage} = require('../pageobjects/LoginPage');
 const {DashboardPage} = require('../pageobjects/DashboardPage');
 const {CartPage} = require('../pageobjects/CartPage');
-const {OrderCompletedPage} = require('../pageobjects/OrderCompletedPage')
+const {CheckOutPage} = require('../pageobjects/CheckOutPage')
 const {MyOrdersPage} = require('../pageobjects/MyOrdersPage');
 
 test.only('Log in Test',async ({page})=> {
@@ -20,9 +20,9 @@ test.only('Log in Test',async ({page})=> {
     const cartPage = new CartPage(page);
     cartPage.confirmProductAnProceedtoCheckout();
     await expect(page.locator(".user__name [type='text']").first()).toHaveText(userEmail);
-    const orderCompletedPage = new OrderCompletedPage(page);
-    await orderCompletedPage.completeShippingInfo();
-    await orderCompletedPage.placeOrder();
+    const checkOutPage = new CheckOutPage(page);
+    await checkOutPage.completeShippingInfo();
+    await checkOutPage.placeOrder();
     await expect(page.locator(".hero-primary")).toHaveText("  Thankyou for the order. ");
     const order = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
     const myOrderPage = new MyOrdersPage(page);
